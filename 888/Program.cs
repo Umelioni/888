945c3c7482ec694c1e9a868eb51c1e74fdde84ec
Umelioni <umelioni001@gmail.com>
2022-08-27 18:01:53 +0200

    first commit

---
+++ 888/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _888
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3);
+        }
+    }
+}
